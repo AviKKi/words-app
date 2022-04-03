f4006d259a26c5e18eff35c790a494b40da8e386
avikki <atul7555@gmail.com>
2022-04-03 21:14:00 +0530

    test file updated

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -1,6 +1,7 @@
 import app from './app'
 const supertest = require("supertest");
 import * as chai from 'chai'
+import { disconnect } from 'mongoose'
 import connectMongo from './provider/mongo';
 import Word from './models/word.model';
 
@@ -16,12 +17,18 @@ beforeEach(async () => {
     await Word.deleteMany({})
 })
 
+afterAll(async () => {
+    await disconnect()
+})
+
 it('POST /api/words/ - should create a new word', async () => {
-    await supertest(app)
+    const res = await supertest(app)
         .post("/api/words/")
         .send({ word: "apple" })
         .expect(201)
+    expect(res.body).to.have.any.keys('word')
+    expect(res.body.word).to.be.equal('apple')
     const words = await Word.find({})
     expect(words).to.be.lengthOf(1)
-    expect(words[0]).to.be.equal("apple")
+    expect(words[0]['word']).to.be.equal("apple")
 })
