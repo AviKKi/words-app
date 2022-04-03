421c048f0dbf84823d6b7750f66cdeb40c8429b3
avikki <atul7555@gmail.com>
2022-04-04 01:15:59 +0530

    Word Card Style updated

--- frontend/src/components/WordCard.tsx
+++ frontend/src/components/WordCard.tsx
@@ -1,12 +1,24 @@
-import { Word } from "../hooks/useWords"
-
+import { Word } from "../hooks/useWords";
+import { FiX, FiEdit3 } from "react-icons/fi";
+import React, { useState } from "react";
 interface WordCardProps {
-    word: Word,
-    children: React.ReactNode
+  word: Word;
+  children: React.ReactNode;
 }
 
-export default function WordCard({word, ...props}: WordCardProps){
-    return <div className="bg-white py-2 my-2 border-2 border-gray-300 rounded-md px-4 hover:border-sky-200">
-        {props.children}
+export default function WordCard({ word, ...props }: WordCardProps) {
+  const [value, setValue] = useState(word.word);
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  const handleUpdate = () => {};
+  const handleDelete = () => {};
+  return (
+    <div className="bg-white py-2 my-2 border-2 border-gray-300 rounded-md px-4 hover:border-sky-200 flex justify-between items-center">
+      <input onBlur={handleUpdate} value={value} onChange={handleValueChange} />
+      <div className="flex gap-2">
+        <FiX onClick={handleDelete} className="cursor-pointer" />
+      </div>
     </div>
+  );
 }
