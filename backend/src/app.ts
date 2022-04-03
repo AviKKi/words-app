321f60056881b3be07f381b0239b7fb43c0abe39
avikki <atul7555@gmail.com>
2022-04-03 20:48:39 +0530

    basic routes defined

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,6 +10,27 @@ app.get("/", (req, res) => {
     res.json({ "msg": "hello" })
 })
 
+// All routes are defined here, for a larger app create seperate routes+controller folder
+// or <app_name>/router+controller
 
+// Create a word
+app.post("/api/words/", (req, res)=>{
+    
+})
+
+// Return list of all the words
+app.get("/api/words/", (req, res)=>{
+
+})
+
+// Delete a word with ID
+app.delete("/api/words/:id/", (req, res)=>{
+
+})
+
+// update a word with ID
+app.put("/api/words/:id/", (req, res)=>{
+
+})
 
 export default app
