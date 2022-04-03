0d20e7afbeaa6e6fcf7f3636dc3fcb96f2e2408c
avikki <atul7555@gmail.com>
2022-04-04 00:55:53 +0530

    test added

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import API from "./api";
+import App from "./App";
+import { Word } from "./hooks/useWords";
 
-test('renders learn react link', () => {
+let words: Word[] = [{ _id: "1", word: "Apple" }];
+const mockAPI = {
+  list: API.list as jest.MockedFunction<typeof API.list>
+}
+
+jest.mock("./api");
+
+beforeEach(() => {
+  mockAPI.list.mockImplementation(async () => words);
+});
+
+test("renders add button", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const addButton = screen.getByText(/add/i);
+  expect(addButton).toBeInTheDocument();
+  await screen.findByTestId("spinner");
+  await waitForElementToBeRemoved(() => screen.queryByTestId(/spinner/i));
 });
